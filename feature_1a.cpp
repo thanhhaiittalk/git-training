fba868ba45a7aab04deee2161a0c2c0d03141e38
thanhhai.ittalk <thanhhai.ittalk@gmail.com>
2024-11-05 14:19:33 +0700

    add feature 1

---
+++ feature_1a.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Function to print the feature name
+void printFeatureName() {
+    printf("Feature 2\n");
+}
+
+int main() {
+    // Call the function to print the feature name
+    printFeatureName();
+
+    return 0;
+}

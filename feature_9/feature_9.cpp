ffa120f1031cf1c36c2b1254ec97db92ef65341d
thanhhai.ittalk <thanhhai.ittalk@gmail.com>
2024-11-05 23:34:26 +0700

    init feature 9

---
+++ feature_9/feature_9.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Print "Hello, World!" to the console
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+
+    printf("Hello, World!\n");
+
+    printf("Hello, World!\n");
+    
+
+    return 0;
+}

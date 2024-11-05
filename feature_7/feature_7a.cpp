ad9718e7aa35b50799477d9df12ec70c1c4f548e
thanhhai.ittalk <thanhhai.ittalk@gmail.com>
2024-11-05 22:06:31 +0700

    add feature_7

---
+++ feature_7/feature_7a.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// Function to add two integers
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int num1 = 5;
+    int num2 = 10;
+
+    // Call the add function and store the result
+    int result = add(num1, num2);
+
+    // Print the result
+    printf("The sum of %d and %d is  need for hot fix%d\n", num1, num2, result);
+    // hot fix
+    return 0;
+}
